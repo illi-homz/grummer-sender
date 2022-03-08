69e6e86244cc507c3aba3a0642cbc3e7079968fb
Ilya Gomza <gopkabel@mail.ru>
2022-03-08 20:49:17 +0300

    add sendPhoto method

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
+from cmath import log
+from json import decoder
 from app import app
 from datetime import datetime
 from flask import  make_response, request
 from flask.json import  jsonify
 
-from .services import massegeSender
+from .services import massegeSender, photoSender
 
 
 @app.route('/')
@@ -14,3 +16,8 @@ def index():
 def send_message():
     data = request.json
     return make_response(jsonify(result=massegeSender(data)), 200)
+
+@app.route('/sendPhoto', methods=['POST'])
+def send_photo():
+    data = request.get_data()
+    return make_response(jsonify(result=photoSender(data)), 200)
--- app/services.py
+++ app/services.py
@@ -5,12 +5,20 @@ import os
 
 botToken = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
-url = 'https://api.telegram.org/bot{botToken}/sendMessage'.format(botToken=botToken)
+url = f'https://api.telegram.org/bot{botToken}/sendMessage'
+url_photo = f'https://api.telegram.org/bot{botToken}/sendPhoto'
 
 def data(msg):
     return {'chat_id': chat_id,'text': msg,'parse_mode': 'markdown'}
 
+def photoData(photo):
+    return {'chat_id': chat_id, 'photo': photo}
+
 
 def massegeSender(msg):
     r = requests.post(url, data(msg))
+    return r.ok
+
+def photoSender(photo):
+    r = requests.post(url_photo, photoData(photo))
     return r.ok
